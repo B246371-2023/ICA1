b82f7fa5d7d869b75c735312210db39ee7841a7f
Sihan Shen <s2530615@bioinfmsc7.bio.ed.ac.uk>
2023-10-23 07:43:12 +0100

    delete a file

--- ICA1/fastq/bowtie.sh
+++
@@ -1,8 +0,0 @@
-for file in $/localdisk/home/s2530615/ICA1/fastq;do
-    if [ -f "$file" ]; then
-        filename="${file##*.}"
-        filename="${filename%_1.fq}"
-        bowtie -x Tcongo_IL3000 -1 "${folder_path}/${filename}_1.fq" -2 "${folder_path}/${filename}_2.fq" -S "${folder_path}/${filename}.bam"
-        echo "Processed: $filename"
-    fi
-done
